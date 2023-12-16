30b467a43fd56ef34abc23b06d53975bdb819e4d
glebus-sasha <glebus-sasha@mail.ru>
2023-12-16 16:57:59 +0300

    add: lm_shiny support

--- scripts/1_eda/lm_shiny.R
+++ scripts/1_eda/lm_shiny.R
@@ -1,3 +1,7 @@
+# Список библиотек для установки, если их нет
+libraries_to_install <- c("shiny", "lmtest", "car", "broom", 
+                          "here")
+
 # Подключение библиотек
 library(shiny)
 library(lmtest)
@@ -5,101 +9,102 @@ library(car)
 library(broom)
 library(here)
 
-# Ваши данные
-path <- here("scripts/0_preprocessing", "preprocessing.R")
-source(path)
-path <- here("data", "Data_SAS.xlsx")
-data_filtered <- preprocessing(path)
 
-# Замена пробелов в названиях переменных подчеркиванием
-names(data_filtered) <- gsub(" ", "_", names(data_filtered))
+lm_shiny <- function(data_path) {
 
-# UI-интерфейс приложения
-ui <- fluidPage(
-  titlePanel("Линейная регрессия в Shiny"),
+  data_filtered <- data_path
   
-  # Боковая панель с выбором предикторов и взаимодействия
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("response_var", "Предсказываемая переменная:",
-                  choices = names(data_filtered),
-                  selected = "Comp_Z",
-                  multiple = FALSE),
-      uiOutput("predictor_selector"),
-      
-      # Выбор взаимодействия
-      checkboxInput("interaction_checkbox", "Взаимодействие факторов", value = FALSE),
-      
-      # Кнопка для сброса предикторов
-      actionButton("reset_button", "Сбросить предикторы")
-    ),
+  # Замена пробелов в названиях переменных подчеркиванием
+  names(data_filtered) <- gsub(" ", "_", names(data_filtered))
+  
+  # UI-интерфейс приложения
+  ui <- fluidPage(
+    titlePanel("Линейная регрессия в Shiny"),
     
-    # Основная панель с выводом регрессионной таблицы
-    mainPanel(
-      tableOutput("regression_table"),
-      textOutput("r_squared_value"),
-      textOutput("adj_r_squared_value")
+    # Боковая панель с выбором предикторов и взаимодействия
+    sidebarLayout(
+      sidebarPanel(
+        selectInput("response_var", "Предсказываемая переменная:",
+                    choices = names(data_filtered),
+                    selected = grep("^Comp", names(data_filtered), value = TRUE)[1],
+                    multiple = FALSE),
+        uiOutput("predictor_selector"),
+        
+        # Выбор взаимодействия
+        checkboxInput("interaction_checkbox", "Взаимодействие факторов", value = FALSE),
+        
+        # Кнопка для сброса предикторов
+        actionButton("reset_button", "Сбросить предикторы")
+      ),
+      
+      # Основная панель с выводом регрессионной таблицы
+      mainPanel(
+        tableOutput("regression_table"),
+        textOutput("r_squared_value"),
+        textOutput("adj_r_squared_value")
+      )
     )
   )
-)
-
-# Серверная часть приложения
-server <- function(input, output, session) {
-  # Реактивное значение для отслеживания выбранных предикторов
-  selected_predictors <- reactiveVal(character(0))
   
-  # Обработка изменений в выборе предикторов
-  observe({
-    selected_predictors(input$predictors)
-  })
-  
-  # Реактивный блок для проведения линейной регрессии
-  regression_model <- reactive({
-    if (length(selected_predictors()) == 0) {
-      # Если не выбран ни один предиктор, строим модель по интерцепту
-      formula_str <- as.formula(paste(input$response_var, "~ 1"))
-    } else {
-      # Добавляем взаимодействие, если выбран соответствующий чекбокс
-      if (input$interaction_checkbox) {
-        formula_str <- as.formula(paste(input$response_var, "~", paste(selected_predictors(), collapse = " * ")))
+  # Серверная часть приложения
+  server <- function(input, output, session) {
+    # Реактивное значение для отслеживания выбранных предикторов
+    selected_predictors <- reactiveVal(character(0))
+    
+    # Обработка изменений в выборе предикторов
+    observe({
+      selected_predictors(input$predictors)
+    })
+    
+    # Реактивный блок для проведения линейной регрессии
+    regression_model <- reactive({
+      if (length(selected_predictors()) == 0) {
+        # Если не выбран ни один предиктор, строим модель по интерцепту
+        formula_str <- as.formula(paste(input$response_var, "~ 1"))
       } else {
-        formula_str <- as.formula(paste(input$response_var, "~", paste(selected_predictors(), collapse = " + ")))
+        # Добавляем взаимодействие, если выбран соответствующий чекбокс
+        if (input$interaction_checkbox) {
+          formula_str <- as.formula(paste(input$response_var, "~", paste(selected_predictors(), collapse = " * ")))
+        } else {
+          formula_str <- as.formula(paste(input$response_var, "~", paste(selected_predictors(), collapse = " + ")))
+        }
       }
-    }
-    lm(formula_str, data = data_filtered)
-  })
+      lm(formula_str, data = data_filtered)
+    })
+    
+    # Вывод результатов линейной регрессии с использованием tidy
+    output$regression_table <- renderTable({
+      tidy_table <- tidy(regression_model())
+      tidy_table
+    })
+    
+    # Вывод R-квадрата
+    output$r_squared_value <- renderText({
+      r_squared <- summary(regression_model())$r.squared
+      paste("R-squared: ", round(r_squared, 4))
+    })
+    
+    # Вывод R-квадрата скорректированного
+    output$adj_r_squared_value <- renderText({
+      adj_r_squared <- summary(regression_model())$adj.r.squared
+      paste("Adjusted R-squared: ", round(adj_r_squared, 4))
+    })
+    
+    # Создание динамического UI для выбора предикторов
+    output$predictor_selector <- renderUI({
+      selectInput("predictors", "Предикторы:",
+                  choices = c("", names(data_filtered)),
+                  selected = selected_predictors(),
+                  multiple = TRUE)
+    })
+    
+    # Обработка сброса предикторов при нажатии кнопки
+    observeEvent(input$reset_button, {
+      selected_predictors(character(0))
+    })
+  }
   
-  # Вывод результатов линейной регрессии с использованием tidy
-  output$regression_table <- renderTable({
-    tidy_table <- tidy(regression_model())
-    tidy_table
-  })
-  
-  # Вывод R-квадрата
-  output$r_squared_value <- renderText({
-    r_squared <- summary(regression_model())$r.squared
-    paste("R-squared: ", round(r_squared, 4))
-  })
-  
-  # Вывод R-квадрата скорректированного
-  output$adj_r_squared_value <- renderText({
-    adj_r_squared <- summary(regression_model())$adj.r.squared
-    paste("Adjusted R-squared: ", round(adj_r_squared, 4))
-  })
-  
-  # Создание динамического UI для выбора предикторов
-  output$predictor_selector <- renderUI({
-    selectInput("predictors", "Предикторы:",
-                choices = c("", names(data_filtered)),
-                selected = selected_predictors(),
-                multiple = TRUE)
-  })
-  
-  # Обработка сброса предикторов при нажатии кнопки
-  observeEvent(input$reset_button, {
-    selected_predictors(character(0))
-  })
+  # Запуск Shiny-приложения
+  shinyApp(ui = ui, server = server)
 }
 
-# Запуск Shiny-приложения
-shinyApp(ui = ui, server = server)
